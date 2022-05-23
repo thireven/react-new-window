31870cd46e05adbe1f5d28f3e4888f2b15457cbd
Thi Nguyen <thnguyen@smartshelf.com>
2022-05-23 08:31:57 -0700

    CSSRules error fix

--- src/NewWindow.js
+++ src/NewWindow.js
@@ -219,58 +219,63 @@ function copyStyles(source, target) {
   // Store style tags, avoid reflow in the loop
   const headFrag = target.createDocumentFragment()
 
-  Array.from(source.styleSheets).forEach(styleSheet => {
-    // For <style> elements
-    let rules
-    try {
-      rules = styleSheet.cssRules
-    } catch (err) {
-      console.error(err)
+  const styleSheets = Array.from(source.styleSheets).filter(
+    (styleSheet) => !styleSheet.href || styleSheet.href.startsWith(window.location.origin)
+  )
+
+  for (let styleSheet of styleSheets) {
+    if (styleSheet instanceof CSSStyleSheet && styleSheet.cssRules) {
+      let rules
+      try {
+        rules = styleSheet.cssRules
+      } catch (err) {
+        console.error(err)
+      }
+      if (rules) {
+        // IE11 is very slow for appendChild, so use plain string here
+        const ruleText = []
+
+        // Write the text of each rule into the body of the style element
+        Array.from(styleSheet.cssRules).forEach(cssRule => {
+          const { type } = cssRule
+
+          // Skip unknown rules
+          if (type === CSSRule.UNKNOWN_RULE) {
+            return
+          }
+
+          let returnText = ''
+
+          if (type === CSSRule.KEYFRAMES_RULE) {
+            // IE11 will throw error when trying to access cssText property, so we
+            // need to assemble them
+            returnText = getKeyFrameText(cssRule)
+          } else if (
+            [CSSRule.IMPORT_RULE, CSSRule.FONT_FACE_RULE].includes(type)
+          ) {
+            // Check if the cssRule type is CSSImportRule (3) or CSSFontFaceRule (5)
+            // to handle local imports on a about:blank page
+            // '/custom.css' turns to 'http://my-site.com/custom.css'
+            returnText = fixUrlForRule(cssRule)
+          } else {
+            returnText = cssRule.cssText
+          }
+          ruleText.push(returnText)
+        })
+
+        const newStyleEl = target.createElement('style')
+        newStyleEl.textContent = ruleText.join('\n')
+        headFrag.appendChild(newStyleEl)
+      } else if (styleSheet.href) {
+        // for <link> elements loading CSS from a URL
+        const newLinkEl = target.createElement('link')
+
+        newLinkEl.rel = 'stylesheet'
+        newLinkEl.href = styleSheet.href
+        headFrag.appendChild(newLinkEl)
+      }
     }
-    if (rules) {
-      // IE11 is very slow for appendChild, so use plain string here
-      const ruleText = []
-
-      // Write the text of each rule into the body of the style element
-      Array.from(styleSheet.cssRules).forEach(cssRule => {
-        const { type } = cssRule
-
-        // Skip unknown rules
-        if (type === CSSRule.UNKNOWN_RULE) {
-          return
-        }
-
-        let returnText = ''
-
-        if (type === CSSRule.KEYFRAMES_RULE) {
-          // IE11 will throw error when trying to access cssText property, so we
-          // need to assemble them
-          returnText = getKeyFrameText(cssRule)
-        } else if (
-          [CSSRule.IMPORT_RULE, CSSRule.FONT_FACE_RULE].includes(type)
-        ) {
-          // Check if the cssRule type is CSSImportRule (3) or CSSFontFaceRule (5)
-          // to handle local imports on a about:blank page
-          // '/custom.css' turns to 'http://my-site.com/custom.css'
-          returnText = fixUrlForRule(cssRule)
-        } else {
-          returnText = cssRule.cssText
-        }
-        ruleText.push(returnText)
-      })
-
-      const newStyleEl = target.createElement('style')
-      newStyleEl.textContent = ruleText.join('\n')
-      headFrag.appendChild(newStyleEl)
-    } else if (styleSheet.href) {
-      // for <link> elements loading CSS from a URL
-      const newLinkEl = target.createElement('link')
-
-      newLinkEl.rel = 'stylesheet'
-      newLinkEl.href = styleSheet.href
-      headFrag.appendChild(newLinkEl)
-    }
-  })
+  }
 
   target.head.appendChild(headFrag)
 }
